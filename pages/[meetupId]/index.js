3d8fe22934eeff996d27173448d82f38effc0d21
unknown <santifp1997@gmail.com>
2023-01-13 03:02:10 -0300

    update

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export const getStaticPaths = async () => {
   client.close();
   console.log(meetups);
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
@@ -62,7 +62,7 @@ export const getStaticProps = async (context) => {
         image: selectedMeetup.image,
       },
     },
-  };x
+  };
 };
 
 export default MeetupDetails;
