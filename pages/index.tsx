5d46dd43f7b12c1c41156ffea15bf1921ff430cb
unknown <santifp1997@gmail.com>
2023-07-15 01:09:01 -0300

    meetups

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,19 @@ interface Props {
 }
 
 const Home = (props: Props) => {
-  return <>
-  <Head>
-    <title>React meetups</title>
-    <meta name="description" content="Hang out"/>
-    <link rel="icon" href="https://hips.hearstapps.com/hmg-prod/images/how-to-keep-ducks-call-ducks-1615457181.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=1200:*" />
-
-  </Head>
-  <MeetupList meetups={props.meetups} />
-  </>
+  return (
+    <>
+      <Head>
+        <title>React meetups</title>
+        <meta name="description" content="Hang out" />
+        <link
+          rel="icon"
+          href="https://hips.hearstapps.com/hmg-prod/images/how-to-keep-ducks-call-ducks-1615457181.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=1200:*"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 };
 
 export const getStaticProps = async () => {
@@ -41,7 +45,7 @@ export const getStaticProps = async () => {
         id: el._id.toString(),
       })),
     },
-    revalidate: 1,
+    revalidate: 5,
   };
 };
 
